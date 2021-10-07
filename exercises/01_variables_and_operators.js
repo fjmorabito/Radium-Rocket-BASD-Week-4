f54504c62ad2eadae47bf96a625193e71ece64e7
Morabito Federico <morabitofj@gmail.com>
2021-10-07 20:58:55 -0300

    BASDW4: exercise 1-a added

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,7 @@
+//                                                Variable y operadores
+
+//a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var var_1 = 15;
+var var_2 = 135;
+var var_3 = var_1 + var_2;
