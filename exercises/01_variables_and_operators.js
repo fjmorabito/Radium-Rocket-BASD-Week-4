4b2eb705ee4aa48bd1cfe23c29173b87209dd109
Morabito Federico <morabitofj@gmail.com>
2021-10-07 20:59:34 -0300

    BASDW4: exercise 1-b added

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -5,3 +5,9 @@
 var var_1 = 15;
 var var_2 = 135;
 var var_3 = var_1 + var_2;
+
+//b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var string_1 = "Morabito ";
+var string_2 = "Federico";
+var string_3 = string_1 + string_2;
