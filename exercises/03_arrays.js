5840011c01f4c72385ba9a04bc3d7f40f03a5c09
Morabito Federico <morabitofj@gmail.com>
2021-10-08 18:20:07 -0300

    BASDW4: exercise 3-a added

---
+++ exercises/03_arrays.js
@@ -0,0 +1,9 @@
+//                                                 Arrays
+
+/* a) Given a whole year month array, show in console the month 5 and 11.*/
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre',
+    'Noviembre', 'Diciembre'];
+
+console.log('The result of exercise 2-f is: ', months[4], ' and ', months[10]);
+
