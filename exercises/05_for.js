f932ff5b5c126f481721b5708f53966b7c782d4c
Morabito Federico <morabitofj@gmail.com>
2021-10-08 20:09:49 -0300

    BASDW4: error not showing on message alert the variable fixed

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -22,7 +22,7 @@ var emptySentence = '';
 for (counter = 0; counter <= fiveWordsArray.length - 1; counter++) {
     emptySentence += (fiveWordsArray[counter]);
 };
-alert('The result of exercise 5-c is: ', emptySentence);
+alert('The result of exercise 5-c is: ' + emptySentence);
 
 /* part d) Create an empty array and with a for loop of 10 repetitions, fill the array with the number of the repetition, that is, at the 
 end of the execution of the for loop, there should be 10 elements within the array, from number 0 to number 9. 
