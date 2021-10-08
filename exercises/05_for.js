fed5e3f52b0ef8f112673c0a9b009504bc0678ae
Morabito Federico <morabitofj@gmail.com>
2021-10-08 19:04:48 -0300

    BASDW4: exercise 5-a added

---
+++ exercises/05_for.js
@@ -0,0 +1,10 @@
+//                                                  For
+
+/*a)  Create an array that contain five words and go over the array using a loop for of JavaScript to show an alert using
+each words.*/
+
+var fiveWordsArray = ["My", "name", "is", "Federico", "Morabito"];
+for (counter = 0; counter < fiveWordsArray.length; counter++) {
+    alert(fiveWordsArray[counter]);
+}
+
