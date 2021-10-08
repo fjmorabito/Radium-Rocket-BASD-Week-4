201b11ced199f50fa31b42367bde792585371194
Morabito Federico <morabitofj@gmail.com>
2021-10-08 17:33:41 -0300

    BASDW4: exercise 2-a added

---
+++ exercises/02_strings.js
@@ -0,0 +1,6 @@
+//                                                   strings
+
+//a) Create a variable string type with at least 10 characters, and change the text into uppercase (use toUppercase).
+
+var shopName = "lee cooper watches"; 
+console.log('The result of exercise 2-a is: ', shopName.toUpperCase());
