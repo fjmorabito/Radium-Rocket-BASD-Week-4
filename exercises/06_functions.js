b5837faf2ff5c76454a3e3e1eb62ca63a598eeb6
Morabito Federico <morabitofj@gmail.com>
2021-10-08 19:35:50 -0300

    BASDW4: exercise 6-a added

---
+++ exercises/06_functions.js
@@ -0,0 +1,11 @@
+//                                                Functions
+
+/*a) Create a sum function that receives two numeric values ​​and returns the result. Execute the function and save the 
+result in a variable, showing the value of said variable in the browser console.*/
+
+function sum(a, b) {
+    return a + b;
+}
+var resultSum = sum(80, 150);
+console.log('The result of exercise 6-a is: ', resultSum);
+
