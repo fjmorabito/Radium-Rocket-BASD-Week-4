fa49c515c55b5647cb0b333e633f03660acd5ac2
Morabito Federico <morabitofj@gmail.com>
2021-10-08 19:39:49 -0300

    BASDW4: exercise 6-b added

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -9,3 +9,21 @@ function sum(a, b) {
 var resultSum = sum(80, 150);
 console.log('The result of exercise 6-a is: ', resultSum);
 
+/*b) To the previous sum function, add a validation to control if any of the parameters is not a number, show an alert 
+clarifying that one of the parameters has an error and return the NaN value as a result.*/
+
+function validateAndSum(a, b) {
+    if (typeof a !== 'number') {
+        alert('The result of exercise 6-b is: the a variable is not a number');
+        return NaN;
+    } else if (typeof b !== 'number') {
+        alert('The result of exercise 6-b is: the b variable is not a number');
+        return NaN;
+    }
+    else {
+        return a + b;
+    }
+}
+var resultAlteredSum = sum(5, 4);
+console.log('The result of exercise 6-b is:', resultAlteredSum);
+
