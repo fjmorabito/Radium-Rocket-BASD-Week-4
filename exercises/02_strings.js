2d4efcc04f0092cfcd8524e1e724664fb0f139d1
Morabito Federico <morabitofj@gmail.com>
2021-10-08 17:45:01 -0300

    BASDW4: added a var in 2-b exercise

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -2,10 +2,11 @@
 
 //a) Create a variable string type with at least 10 characters, and change the text into uppercase (use toUppercase).
 
-var shopName ='leecoopers watches'; 
+var shopName = 'leecoopers watches';
 console.log('The result of exercise 2-a is: ', shopName.toUpperCase());
 
 //b) Create a string type variable with at least 10 characters and generate a new one with the first 5 characters from it.
 
-var shopName5Characthers = shopName.substring(0 , 5);
+var newShopName = 'efficiently sport'
+var shopName5Characthers = newShopName.substring(0, 5);
 console.log('The result of exercise 2-b is: ', shopName5Characthers);
