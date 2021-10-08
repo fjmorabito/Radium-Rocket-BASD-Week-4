743b1cf997921d26923b0dea0aa74df1d7d51570
Morabito Federico <morabitofj@gmail.com>
2021-10-07 21:03:13 -0300

    BASDW4: correct the use of '' instead of . Change name on some variables

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -8,12 +8,12 @@ var var_3 = var_1 + var_2;
 
 //b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
-var string_1 = "Morabito ";
-var string_2 = "Federico";
+var string_1 = 'Morabito ';
+var string_2 = 'Federico';
 var string_3 = string_1 + string_2;
 
 //c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 
-var string_1 = "Morabito ";
-var string_2 = "Federico";
-var string_3 = string_1.length + string_2-length;
+var string_4 = 'Morabito ';
+var string_5 = 'Federico';
+var string_6 = string_4.length + string_5.length;
